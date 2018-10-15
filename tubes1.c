7a8e5741bff59ad1c0957ef136e1988a224c4214
Louis Cahyadi <louiscahyadi23@gmail.com>
2018-10-15 23:22:35 +0700

    Update tubes1.c

--- tubes1.c
+++ tubes1.c
@@ -2,10 +2,11 @@
 #include <string.h>
 #include <stdbool.h>
 
+//Membuat tipe bentukan state yang merupakan array of char
 typedef struct {
     char T[10];
 } state;
-
+//Deklarasi array untuk menampung state-state yang telah dilewati
 state arrOfState[100];
 
 void menuAwal();
@@ -14,69 +15,58 @@ bool isSeri(state q);
 void tulisState(state q);
 void pindahState(state *q, int i);
 
-
 int main(){
     int pilihan;
-    int i;
-    int j;
+    int i,j;
     state qSekarang; //Menampung state yang sedang terjadi sekarang
     state q;
 
-
     menuAwal();
-    printf("\n>>");
+    printf("\n>> ");
     scanf("%d",&pilihan);
-    if (pilihan == 1){ //Komputer jalan duluan
-        //Buat qSekarang
-        qSekarang.T[0] = 'A';
-        qSekarang.T[1] = 'A';
-        qSekarang.T[2] = 'A';
-        qSekarang.T[3] = 'A';
-        qSekarang.T[4] = 'O';
-        qSekarang.T[5] = 'A';
-        qSekarang.T[6] = 'A';
-        qSekarang.T[7] = 'A';
-        qSekarang.T[8] = 'A';
-        qSekarang.T[9]= '\0';
-    } else {
-        //Buat qSekarang
-        qSekarang.T[0] = 'A';
-        qSekarang.T[1] = 'A';
-        qSekarang.T[2] = 'A';
-        qSekarang.T[3] = 'A';
-        qSekarang.T[4] = 'X';
-        qSekarang.T[5] = 'A';
-        qSekarang.T[6] = 'O';
-        qSekarang.T[7] = 'A';
-        qSekarang.T[8] = 'A';
-        qSekarang.T[9]= '\0';
-    }
+
+    //inisiasi start state
+    qSekarang.T[0] = 'A';
+    qSekarang.T[1] = 'A';
+    qSekarang.T[2] = 'A';
+    qSekarang.T[3] = 'A';
+    qSekarang.T[4] = 'A';
+    qSekarang.T[5] = 'A';
+    qSekarang.T[6] = 'A';
+    qSekarang.T[7] = 'A';
+    qSekarang.T[8] = 'A';
+    qSekarang.T[9] = '\0';
+
+    //masukan pertama
+    pindahState(&qSekarang,pilihan);
 
     tulisState(qSekarang);
     arrOfState[1] = qSekarang;
     j = 2;
 
     while (!isMenang(qSekarang) && !isSeri(qSekarang)){
-        printf("Giliran Anda : ");
+        printf("\nMasukan nomor kotak yang ingin Anda beri tanda : ");
         scanf("%d",&i);
 
         pindahState(&qSekarang,i);
         tulisState(qSekarang);
         arrOfState[j] = qSekarang;
         j += 1;
-        printf("\n");
-    }
-
+    } //isMenang = true atau isSeri = true
+    //Menampilkan status akhir permainan
     if (isMenang(qSekarang)){
-        printf("Permainan selesai, Komputer menang, cupu lu\n");
+        printf("\nPermainan telah selesai dan dimenangkan oleh komputer\n\n");
     } else if (isSeri(qSekarang)){
-        printf("Permainan selesai, imbang coy\n");
+        printf("\nPermainan telah selesai dengan imbang\n\n");
     }
-
+    //Menampilkan daftar state yang telah dilewati
+    printf("Berikut ini ialah daftar state yang telah dilewati :\n");
     for (i = 1;i < j;i++){
-        printf("%s\t",arrOfState[i]);
+        printf("%d. %s\n",i,arrOfState[i]);
     }
 
+    printf("\nKetik sembarang angka untuk keluar : ");
+    scanf("%d",&j);
     return 0;
 }
 
@@ -85,30 +75,29 @@ void menuAwal(){
     printf("---------------------------------\n");
     printf("---------- TIC TAC TOE ----------\n");
     printf("---------------------------------\n");
-    printf("\n");
-    printf("Siapa yang mau bermain duluan ?\n1.Komputer\n2.User\n");
+    printf("");
+    printf("Siapa yang akan main pada giliran pertama?\n1. Komputer\n2. Anda\n");
 }
 
 bool isMenang(state q){
-//Mengembalikan true bila state q merupakan final state dan dalam kondisi komputer
-//menang
+//Mengembalikan true bila state q merupakan final state dan dalam kondisi komputer menang
     FILE *pFile;
-    char stateSebaris[100];
-    char stateMenang[10] = "WWWWWWWWW";
+    char stateSebaris[110];
+    char stateMenang[2] = "1";
     state qcek;
     state q1;
 
     pFile = fopen ("state.txt", "r");
     while (!feof(pFile)){
-        if (fgets(stateSebaris, 101, pFile) == NULL){
+        if (fgets(stateSebaris, 111, pFile) == NULL){
             break;
         } else {
             memcpy(qcek.T, &stateSebaris[0],9);
             qcek.T[9] = '\0';
 
             if (strcmp(qcek.T,q.T) == 0){
-                memcpy(q1.T, &stateSebaris[10],9);
-                q1.T[9] = '\0';
+                memcpy(q1.T, &stateSebaris[9],1);
+                q1.T[1] = '\0';
                 if (strcmp(q1.T,stateMenang) == 0){
                     return true;
                 } else {
@@ -121,25 +110,24 @@ bool isMenang(state q){
 }
 
 bool isSeri(state q){
-//Mengembalikan true apabila state q merupakan final state dan dalam kondisi
-//permainan berakhir imbang.
+//Mengembalikan true apabila state q merupakan final state dan dalam kondisi permainan berakhir imbang.
     FILE *pFile;
-    char stateSebaris[100];
-    char stateSeri[10] = "SSSSSSSSS";
+    char stateSebaris[110];
+    char stateSeri[2] = "2";
     state qcek;
     state q1;
 
     pFile = fopen ("state.txt", "r");
     while (!feof(pFile)){
-        if (fgets(stateSebaris, 101, pFile) == NULL){
+        if (fgets(stateSebaris, 111, pFile) == NULL){
             break;
         } else {
             memcpy(qcek.T, &stateSebaris[0],9);
             qcek.T[9] = '\0';
 
             if (strcmp(qcek.T,q.T) == 0){
-                memcpy(q1.T, &stateSebaris[10],9);
-                q1.T[9] = '\0';
+                memcpy(q1.T, &stateSebaris[9],1);
+                q1.T[1] = '\0';
                 if (strcmp(q1.T,stateSeri) == 0){
                     return true;
                 } else {
@@ -160,7 +148,7 @@ void tulisState(state q){
         }
     }
 
-    printf("%c | %c | %c\n",q.T[0],q.T[1],q.T[2]);
+    printf("\n%c | %c | %c\n",q.T[0],q.T[1],q.T[2]);
     printf("----------\n");
     printf("%c | %c | %c\n",q.T[3],q.T[4],q.T[5]);
     printf("----------\n");
@@ -168,23 +156,23 @@ void tulisState(state q){
 }
 
 void pindahState(state *q, int i){
-//Merubah state q menuju state jika diinput i
+//Merubah state q ke state yang dituju jika diberi input i
     FILE *pFile;
     int j;
-    char stateSebaris[100];
+    char stateSebaris[110];
     state qcek;
     state qi;
 
     pFile = fopen ("state.txt", "r");
     while (!feof(pFile)){
-        if (fgets(stateSebaris, 101, pFile) == NULL){
+        if (fgets(stateSebaris, 111, pFile) == NULL){
             break;
         } else {
             memcpy(qcek.T, &stateSebaris[0],9);
             qcek.T[9] = '\0';
 
             if (strcmp(qcek.T,(*q).T) == 0){
-                memcpy(qi.T, &stateSebaris[10*i],9);
+                memcpy(qi.T, &stateSebaris[11*i],9);
                 qi.T[9] = '\0';
 
                 *q = qi;
